a15ccea3f64275c0029e1f868252d218422363b9
evanFullerRUNI <evan.fuller@post.runi.ac.il>
2024-01-10 13:33:00 +0200

    HW3 calendar0

--- Calendar0.java
+++ Calendar0.java
@@ -22,13 +22,22 @@ public class Calendar0 {
 
 	// Tests the nDaysInMonth function.
 	private static void nDaysInMonthTest(int year) {
-		// Replace this comment with your code
+		for (int i = 1; i <= 12; i++) {
+			System.out.println("Month " + i + " has " + nDaysInMonth(i, year) + " days");
+		}
 	}
 
 	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
-	    // Replace the following statement with your code
-		return false;
+	    boolean isLeap = false;
+		if (year % 400 == 0) {
+			isLeap = true;
+		} else if (year % 100 == 0) {
+			isLeap = false;
+		} else if (year % 4 == 0) {
+			isLeap = true;
+		}
+		return isLeap;
 	}
 	 
 	// Returns the number of days in the given month and year.
@@ -37,7 +46,21 @@ public class Calendar0 {
 	// All the other months have 31 days.
 	public static int nDaysInMonth(int month, int year) {
 		// Replace the following statement with your code
-		return 0;
+		switch (month) {
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				return 30;
+			case 2:
+				if (isLeapYear(year)) {
+					return 29;
+				} else {
+					return 28;
+				}
+			default:
+				return 31;
+		}
 	}
 }
 
